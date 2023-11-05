91d69406b336a9bdb6b0290bbb5a8e62c99fc744
Juwel <developerjuwel792@gmail.com>
2023-11-05 20:41:49 +0600

    hexadecimal

--- random rgb.js
+++ random rgb.js
@@ -1,12 +1,21 @@
 const container = document.getElementById("rgb-container")
-console.log("🚀 ~ file: random rgb.js:2 ~ container:", container)
 const btn = document.getElementById("rgb-btn");
+const output = document.getElementById("output");
+console.log("🚀 ~ file: random rgb.js:4 ~ output:", output)
+
 
 
 btn.addEventListener("click", () => {
-    const r = Math.round(Math.random() * 255)
-    const g = Math.round(Math.random() * 255)
-    const b = Math.round(Math.random() * 255)
+    const r = Math.round(Math.random() * 255).toString(16)
+    const g = Math.round(Math.random() * 255).toString(16)
+    const b = Math.round(Math.random() * 255).toString(16)
+    const rgb = `#${r}${g}${b}`
 
-    container.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
+    container.style.backgroundColor = rgb;
+    output.value = rgb
+
 })
+
+// rgb(255, 255, 255)
+// #000000
+// red
